112c47620f62617fadb93b8cb00bf4d8f5ad4c1f
StephenG98 <txsteven@gmail.com>
2020-08-06 15:33:17 -0500

    finished CommentsE2EHappyPath

--- src/test/groovy/E2E/CommentsE2EHappyPath.groovy
+++ src/test/groovy/E2E/CommentsE2EHappyPath.groovy
@@ -1,21 +1,32 @@
 package E2E
 
+import ClientData.CommentData
+import ClientData.PostData
 import groovyx.net.http.RESTClient
+import spock.lang.Shared
 import spock.lang.Specification
 
 import static groovyx.net.http.ContentType.JSON
 
 class CommentsE2EHappyPath extends Specification {
 
-    def client = new RESTClient('https://jsonplaceholder.typicode.com/')
+    @Shared PostData postData
+    @Shared CommentData data
+    @Shared def client
+
+    def setupSpec() {
+        client = new RESTClient('https://jsonplaceholder.typicode.com/')
+        postData = new PostData()
+        data = new CommentData()
+    }
 
     def 'user can create a new post' () {
         when:
         def response = client.post(path: 'posts',
                 contentType: JSON,
                 body: [userId: '1',
-                       titles: 'title1',
-                       body  : 'body1'])
+                       title: postData.getPostTitle(),
+                       body  : postData.getPostBody()])
         then:
         assert response.status == 201: 'The new post was not created.'
     }
@@ -23,59 +34,62 @@ class CommentsE2EHappyPath extends Specification {
     def 'get post' () {
         when:
         def response = client.get(path: 'posts/1')
-//        def response = getData()
 
         then:
         assert response.status == 200 : 'Post was not retrieved.'
+        //no need to check for data inside GET for a post since that is taken care of in PostsE2EHappyPath
     }
 
     def 'add a comment to the post' () {
         when:
-        def response = client.post(path: 'posts/1/comments',
+        def response = client.post(path: 'posts/' + postData.getPostId() + '/comments',
                 contentType: JSON,
-                bidy: ['postId': 1,
-                       'id': 1,
-                       'name': "meaningful name",
-                       'email': "Eliseo@gardner.biz",
-                       'body': "meaningful body"])
+                body: ['postId': data.getCommentPostId(),
+                       'id': data.getCommentId(),
+                       'name': data.getCommentName(),
+                       'email': data.getCommentEmail(),
+                       'postBody': data.getCommentBody()])
 
         then:
         assert response.status == 201 : 'Comment was not added.'
     }
 
+    // This currently fails cause of the \n escape character problem for the "body" key inside the  /comments API
     def 'get comment' () {
         when:
-        def response = client.get(path: 'posts/1/comments')
+        def response = client.get(path: 'posts/' + postData.getPostId() + '/comments')
 
         then:
-        assert response.status == 200 : 'Comment was not retrieved.'
+        checkForCorrectDataInCommentResponse(response, 200, data.getCommentId(),
+                data.getCommentName(), data.getCommentEmail(), data.getCommentBody())
     }
 
     def 'edit comment' () {
         when:
-        def response = client.put(path: 'posts/1/comments',
+        def response = client.put(path: 'posts/' + postData.getPostId() + '/comments',
                 contentType: JSON,
-                bidy: ['postId': 1,
-                       'id': 1,
-                       'name': "another meaningful name",
-                       'email': "Eliseo@gardner.biz",
-                       'body': "another meaningful body"])
+                body: ['postId': data.getCommentPostId(),
+                       'id': data.getCommentId(),
+                       'name': data.getEditedCommentName(),
+                       'email': data.getEditedCommentEmail(),
+                       'postBody': data.getEditedCommentBody()])
         then:
         assert response.status == 200 : 'The new post was not updated.'
     }
 
     def 'get edited comment' () {
         when:
-        def response = client.get(path: 'posts/1/comments')
+        def response = client.get(path: 'posts/' + postData.getPostId() + '/comments')
 
         then:
-        assert response.status == 200 : 'Comment was not retrieved.'
+        checkForCorrectDataInCommentResponse(response, 200, data.getCommentId(),
+                data.getEditedCommentName(), data.getEditedCommentEmail(), data.getEditedCommentBody())
     }
 
     def 'delete comment' () {
         //apparently there is no func. to delete comments https://jsonplaceholder.typicode.com/posts/1/comments/1
         when:
-        def response = client.delete(path: 'posts/1/comments/1')
+        def response = client.delete(path: 'posts/' + postData.getPostId() + '/comments/' + data.getCommentId())
 
         then:
         assert response.status == 204 : 'Comment was not deleted.'
@@ -83,16 +97,18 @@ class CommentsE2EHappyPath extends Specification {
 
     def 'validate comment is gone' () {
         when:
-//        def response = getData()
-        def response = client.get(path: 'posts/1/comments/1')
+        def response = client.get(path: 'posts/' + postData.getPostId() + '/comments/' + data.getCommentId())
 
         then:
-        assert response.status == 404 : 'Deleted post was invalidly retrieved'
+        assert response.status == 404 : 'Deleted comment was invalidly retrieved'
     }
 
-//    def getData() {
-//        def response = client.get(path: 'posts/1')
-//        return response
-//    }
-
+    void checkForCorrectDataInCommentResponse(response, responseStatus, commentId, commentName, commentEmail,
+                                              commentBody) {
+        assert response.status == responseStatus : 'The expected status code is incorrect'
+        assert response['data']['id'][data.getCommentId() - 1] == commentId : 'The expected commentID is incorrect'
+        assert response['data']['name'][data.getCommentId() - 1] == commentName : 'The expected commentName is incorrect'
+        assert response['data']['email'][data.getCommentId() - 1] == commentEmail : 'The expected commentEmail is incorrect'
+        assert response['data']['body'][data.getCommentId() - 1] == commentBody : 'The expected commentBody is incorrect'
+    }
 }
